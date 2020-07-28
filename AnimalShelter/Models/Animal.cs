ad6c3d57e653b65d5e7afc9766c2b0e1bc117479
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-07-28 14:29:28 -0700

    add type property to animal.cs
    Co-authored-by: fetonecontrol <frederick.ernest@gmail.com>
    Co-authored-by: Thomas Glenn <glenaissance@yahoo.com>

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -8,16 +8,15 @@ namespace AnimalShelter.Models
     public int Id { get; set; }
     public string Description { get; set; }
 
-    // public string Type { get; set; }
+    public string Type { get; set; }
 
-    // public string Name { get; set; }
+    public string Name { get; set; }
 
-    // public string Gender { get; set; }
+    public string Gender { get; set; }
 
-    // public string Breed { get; set; }
+    public string Breed { get; set; }
 
-    // public string Date { get; set; }
-
+    public string Date { get; set; }
 
   }
 }
